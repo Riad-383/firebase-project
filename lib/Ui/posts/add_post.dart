0cb2628cd0793bd5ef6c34549a212646e074a54b
Riad <mriad0096@gmail.com>
2023-08-09 19:57:22 +0600

    6th commit

--- lib/Ui/posts/add_post.dart
+++ lib/Ui/posts/add_post.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_project/utils/utils.dart';
+import 'package:firebase_project/widgets/round_button.dart';
 import 'package:flutter/material.dart';
 
 class AddPost extends StatefulWidget {
@@ -8,8 +11,68 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  final databaseRef = FirebaseDatabase.instance.ref('Post');
+  bool loading = false;
+
+  final postCtrl = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add post"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              controller: postCtrl,
+              maxLines: 4,
+              decoration: const InputDecoration(
+                hintText: 'Whats on your mind..?',
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            RoundButton(
+                loading: loading,
+                title: "Post",
+                onTap: () {
+                  setState(() {
+                    loading = true;
+                  });
+
+                  String id=DateTime.now().millisecondsSinceEpoch.toString();
+                  databaseRef
+                      .child(id)
+                      .set({'title': postCtrl.text.toString(), 
+                      'id': id,
+                      }).then(
+                          (value) {
+                             
+                    Utils().toastMassage('Post added');
+
+                    setState(() {
+                    loading = false;
+                  });
+                  
+                  }).onError((error, stackTrace) {
+
+                    
+                    Utils().toastMassage(error.toString());
+
+                     setState(() {
+                    loading = false;
+                  });
+                  });
+                })
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Ui/posts/post_screen.dart
+++ lib/Ui/posts/post_screen.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_project/Ui/posts/add_post.dart';
 import 'package:firebase_project/Ui/uth/login_screen.dart';
 import 'package:firebase_project/utils/utils.dart';
@@ -13,6 +15,9 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref('Post');
+  final searchCtrl = TextEditingController();
+  final showdialogctrl = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +39,75 @@ class _PostScreenState extends State<PostScreen> {
               icon: const Icon(Icons.logout_outlined))
         ],
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TextFormField(
+              controller: searchCtrl,
+              decoration: const InputDecoration(
+                  hintText: 'Search', border: OutlineInputBorder()),
+              onChanged: (value) {
+                setState(() {});
+              },
+            ),
+          ),
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: ref,
+                itemBuilder: ((context, snapshot, animation, index) {
+                  String title = snapshot.child('title').value.toString();
+
+                  if (searchCtrl.text.isEmpty) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                      trailing: PopupMenuButton(
+                        icon: const Icon(Icons.more_vert),
+                        itemBuilder: (context) => [
+                          PopupMenuItem(
+                              value: 1,
+                              child: ListTile(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  showMyDialouge(title,
+                                      snapshot.child('id').value.toString());
+                                },
+                                leading: const Icon(Icons.edit),
+                                title: const Text('Edit'),
+                              )),
+                          PopupMenuItem(
+                              onTap: () {
+                                Navigator.pop(context);
+                                ref
+                                    .child(
+                                        snapshot.child('id').value.toString())
+                                    .remove();
+                              },
+                              child: const ListTile(
+                                leading: Icon(Icons.delete),
+                                title: Text('Delete'),
+                              ))
+                        ],
+                      ),
+                    );
+                  } else if (title
+                      .toLowerCase()
+                      .contains(searchCtrl.text.toLowerCase())) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                    );
+                  } else {
+                    return Container();
+                  }
+                })),
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -44,6 +118,43 @@ class _PostScreenState extends State<PostScreen> {
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Future<void> showMyDialouge(String title, String id) async {
+    showdialogctrl.text = title;
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Update'),
+          content: Container(
+            child: TextFormField(
+              controller: showdialogctrl,
+              decoration: const InputDecoration(hintText: 'Edit post'),
+            ),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel")),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  ref.child(id).update({
+                    "title": showdialogctrl.text.toLowerCase(),
+                  }).then((value) {
+                    Utils().toastMassage('Post Updated');
+                  }).onError((error, stackTrace) {
+                    Utils().toastMassage(error.toString());
+                  });
+                },
+                child: const Text("Update"))
+          ],
+        );
+      },
     );
   }
 }
